5896e942365e5035c18952a1f522c26bc6902fab
S4sunny <srocks023@gmail.com>
2023-01-24 12:11:44 +0530

    Ingestion Part done

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from Insurance.logger import logging
 from Insurance.exception import InsuranceException
 from Insurance.utils import get_collection_as_dataframe
+from Insurance.entity import config_entity
+from Insurance.entity.config_entity import DataIngestionConfig
+
 import sys, os
 
 if __name__=="__main__":
      try:
-        get_collection_as_dataframe(database_name="INSURANCE",collection_name="INSURANCE_PROJECT")
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
      except Exception as e:
           print(e)    
