2f75320ef5ffa0006dc91d599e45e87174e6b209
S4sunny <srocks023@gmail.com>
2023-01-26 12:56:06 +0530

    Data Transformation

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
 from Insurance.components.data_ingestion import DataIngestion
 from Insurance.components.data_validation import DataValidation
+from Insurance.components.data_transformation import DataTransformation
+
 
 if __name__=="__main__":
      try:
@@ -26,6 +28,14 @@ if __name__=="__main__":
                          data_ingestion_artifact=data_ingestion_artifact)
         
         data_validation_artifact = data_validation.initiate_data_validation()
+
+        #Data Transformation
+
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+        data_ingestion_artifact=data_ingestion_artifact)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+
 
 
      except Exception as e:
