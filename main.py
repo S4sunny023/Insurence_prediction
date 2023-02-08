169dddc4cb2e5b06e9e331ec08d1f011f8675cdb
S4sunny <srocks023@gmail.com>
2023-02-08 18:39:34 +0530

    model evaluation done

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from Insurance.components.data_ingestion import DataIngestion
 from Insurance.components.data_validation import DataValidation
 from Insurance.components.model_trainer import ModelTrainer
 from Insurance.components.data_transformation import DataTransformation
+from Insurance.components.model_evaluation import ModelEvaluation
 
 
 
@@ -43,7 +44,17 @@ if __name__=="__main__":
        model_trainer_artifact = model_trainer.initiate_model_trainer()
 
 
-     
+       # Model Evaluation
+       model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config = training_pipeline_config)
+       model_eval = ModelEvaluation(model_eval_config = model_eval_config,
+       data_ingestion_artifact = data_ingestion_artifact,
+       data_transformation_artifact = data_transformation_artifact,
+       model_trainer_artifact = model_trainer_artifact)
+       model_evl_artifact = model_eval.initiate_model_evaluation()
 
      except Exception as e:
-          print(e)
+          print(e)   
+
+
+
+     
